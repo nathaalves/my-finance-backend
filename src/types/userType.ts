b36798abfb6bd943fbf1e38c330108f7863a5f79
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-11-21 22:28:03 -0300

    feat: create UserData type

--- src/types/userType.ts
+++ src/types/userType.ts
@@ -5,4 +5,6 @@ export interface CreateUser extends Pick<User, 'email' | 'name'> {
   confirm_password: string;
 }
 
+export type UserData = Omit<CreateUser, 'confirm_password'>;
+
 export type insertUser = Pick<User, 'email' | 'name' | 'password_hash'>;
