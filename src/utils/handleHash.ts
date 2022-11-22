9d947c4e06059ea37c7318ac2415bc0f69444ea4
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-11-21 22:27:41 -0300

    feat: implements createHash function

---
+++ src/utils/handleHash.ts
@@ -0,0 +1,14 @@
+import { genSaltSync, hashSync } from 'bcrypt';
+import 'dotenv/config';
+
+function createHash(data: string) {
+  const SALT_ROUNDS = Number(process.env.BCRYPT_SALT_ROUNDS) || 12;
+  console.log(SALT_ROUNDS);
+  const salt = genSaltSync(SALT_ROUNDS);
+
+  const encryptedData = hashSync(data, salt);
+
+  return encryptedData;
+}
+
+export { createHash };
