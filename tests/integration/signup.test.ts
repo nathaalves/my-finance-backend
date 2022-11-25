a96fc91ca0c1f66b0d1f5dc783b50b9feace72af
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-11-25 16:04:13 -0300

    test: configure DB conections

--- tests/integration/signup.test.ts
+++ tests/integration/signup.test.ts
@@ -1,8 +1,19 @@
 import { signupInformations } from '../factories/userFactory';
 import { app } from '../../src/app';
+import { initDB, disconnectDB } from '../../src/config/database';
+import { cleanDb } from '../cleanDB';
 import supertest from 'supertest';
 
 const request = supertest(app);
+
+beforeAll(async () => {
+  await initDB();
+  await cleanDb();
+});
+
+afterEach(async () => {
+  await cleanDb();
+});
 
 describe('Register', () => {
   it('should create a new user sending valid informations', async () => {
@@ -12,4 +23,8 @@ describe('Register', () => {
 
     expect(result.status).toBe(201);
   });
+});
+
+afterAll(async () => {
+  await disconnectDB();
 });
