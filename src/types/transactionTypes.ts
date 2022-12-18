3542e207a84b7cdcee3f1edf53be941f03e89d3d
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-18 12:22:30 -0300

    feat: update transaction types

--- src/types/transactionTypes.ts
+++ src/types/transactionTypes.ts
@@ -1,9 +1,11 @@
 import { Transaction } from '@prisma/client';
 
-export interface TransactionBody
-  extends Omit<Transaction, 'id' | 'createdAt' | 'updatedAt' | 'userId'> {}
+export type TransactionBody = Omit<
+  Transaction,
+  'id' | 'createdAt' | 'updatedAt' | 'userId'
+>;
 
-export interface InsertTransaction
-  extends Omit<Transaction, 'id' | 'createdAt' | 'updatedAt' | 'date'> {
-  date: Date;
-}
+export type InsertTransaction = Omit<
+  Transaction,
+  'id' | 'createdAt' | 'updatedAt'
+>;
