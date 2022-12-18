f008623a4c807167cf5e1b25d8ad4af13afa546b
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-18 11:26:28 -0300

    feat: create schema for delete transaction

--- src/schemas/transactionSchema.ts
+++ src/schemas/transactionSchema.ts
@@ -1,10 +1,16 @@
 import joi from 'joi';
 import { TransactionBody } from '../types/transactionTypes';
 
-export const transactionSchema = joi.object<TransactionBody>({
+const addTransaction = joi.object<TransactionBody>({
   description: joi.string().required(),
   note: joi.string(),
   value: joi.number().required(),
   categoryId: joi.string().required(),
   date: joi.date().required(),
 });
+
+const deleteTransaction = joi.object({
+  id: joi.string().uuid().required(),
+});
+
+export const transactionSchema = { addTransaction, deleteTransaction };
