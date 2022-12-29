70d734bba2a3c0e2e520b4a4301c8904e76f32e7
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-28 22:45:02 -0300

    refactor: reuse load env logic

--- src/utils/handleToken.ts
+++ src/utils/handleToken.ts
@@ -1,12 +1,21 @@
 import { sign, verify } from 'jsonwebtoken';
 import { BusinessRuleError } from '../Errors';
-import { JWTPayload } from '../types/userType';
+import { JWTPayload } from '../types/userTypes';
 
-function generateToken(
-  payload: JWTPayload,
-  secretKey: string | undefined,
-  expiresIn: string | undefined
-) {
+function loadEnvs(type: 'access' | 'refresh') {
+  let secretKey: string | undefined;
+  let expiresIn: string | undefined;
+
+  if (type === 'access') {
+    secretKey = process.env.JWT_ACCESS_TOKEN_SECRET_KEY;
+    expiresIn = process.env.JWT_ACCESS_TOKEN_EXPIRES_IN;
+  }
+
+  if (type === 'refresh') {
+    secretKey = process.env.JWT_REFRESH_TOKEN_SECRET_KEY;
+    expiresIn = process.env.JWT_REFRESH_TOKEN_EXPIRES_IN;
+  }
+
   if (!secretKey) {
     throw new BusinessRuleError(
       'É necessário a definição de uma chave secreta para a geração do token.',
@@ -21,17 +30,26 @@ function generateToken(
     );
   }
 
+  return { secretKey, expiresIn };
+}
+
+function generateToken(payload: JWTPayload) {
+  const { secretKey, expiresIn } = loadEnvs(payload.type);
+
   return sign(payload, secretKey, { expiresIn });
 }
 
-function validateToken(token: string, secretKey: string) {
-  try {
-    const payload = verify(token, secretKey);
+function validateToken(token: string, type: 'access' | 'refresh') {
+  const { secretKey } = loadEnvs(type);
 
-    return payload;
-  } catch (error) {
-    throw new BusinessRuleError('Token inválido.', 401);
-  }
+  const payload = verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      throw new BusinessRuleError('Token expirado ou inválido.', 498);
+    }
+    return decoded;
+  });
+
+  return payload as unknown as JWTPayload;
 }
 
 export { generateToken, validateToken };
