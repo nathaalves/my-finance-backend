8cd31c1b9b3cf724cd6ec0a6ff39c70b012217fd
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-28 22:58:51 -0300

    feat: prepare app to handle cookies

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { router } from './routes/router';
 import { errorHandler } from './middlewares/errorHandler';
+import { options } from './config/cors';
+import { validateCredentials } from './middlewares/validateCredentials';
 
 const app = express();
 
-app.use(cors());
+app.use(validateCredentials);
+
+app.use(cors(options));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(router);
+
 app.use(errorHandler);
 
 export { app };
