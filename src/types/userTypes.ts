4a6c72a1e5667feb9374591b8ca71a2148b49f6a
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-28 22:46:25 -0300

    feat: add type to JWTPayload

---
+++ src/types/userTypes.ts
@@ -0,0 +1,20 @@
+import type { User } from '@prisma/client';
+
+export type Signup = Pick<User, 'email' | 'name'> & {
+  password: string;
+  confirm_password: string;
+};
+
+export type UserData = Omit<Signup, 'confirm_password'>;
+
+export type insertUser = Pick<User, 'email' | 'name' | 'password_hash'>;
+
+export type Signin = Pick<User, 'email'> & {
+  password: string;
+};
+
+export type JWTPayload = {
+  id: string;
+  name: string;
+  type: 'access' | 'refresh';
+};
