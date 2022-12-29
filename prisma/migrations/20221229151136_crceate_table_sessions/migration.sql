ff3f5ccc5fd8fe2fd57fdf308eddf10bd834af1f
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2022-12-29 12:12:08 -0300

    feat: create table sessions

---
+++ prisma/migrations/20221229151136_crceate_table_sessions/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
