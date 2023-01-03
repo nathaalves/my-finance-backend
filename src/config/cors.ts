55c193cc76ea5e0c9a166667e8e8829d05a98580
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-01-03 18:45:13 -0300

    fix: fix origins

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -6,8 +6,8 @@ export const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:5173',
   'https://my-finance-frontend.vercel.app',
-  'https://my-finance-frontend-nathaalves.vercel.app/',
-  'https://my-finance-frontend-git-main-nathaalves.vercel.app/',
+  'https://my-finance-frontend-nathaalves.vercel.app',
+  'https://my-finance-frontend-git-main-nathaalves.vercel.app',
 ];
 
 export const options = {
