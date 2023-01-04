1517cf94ecef8c1bbbf2e5794335f0aecc57f097
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-01-04 19:33:29 -0300

    feat: add origin

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,7 @@ export const allowedOrigins = [
   `http://localhost:${process.env.PORT || 5000}`,
   'http://127.0.0.1:5173',
   'http://localhost:5173',
+  'https://myfinance.app.br',
   'https://my-finance-frontend.vercel.app',
   'https://my-finance-frontend-nathaalves.vercel.app',
   'https://my-finance-frontend-git-main-nathaalves.vercel.app',
