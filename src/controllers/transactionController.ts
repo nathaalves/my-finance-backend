35543739c27f1cf7439bf465b80c52c14e3972d3
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-01-04 22:12:06 -0300

    fix: remove console.log

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -5,7 +5,7 @@ import { TransactionBody } from '../types/transactionTypes';
 async function createTransaction(req: Request, res: Response) {
   const body = req.body as TransactionBody;
   const { userId } = res.locals.payload;
-  console.log(body.date);
+
   const transaction = await transactionService.insertData(body, userId);
 
   res.status(201).send(transaction);
